70b96e08a4ba6d568e3d40b691433d185ab0de78
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-04-07 01:12:17 +0530

    updates

---
+++ 05-04-2024/http.js
@@ -0,0 +1,39 @@
+// const http = require("http")
+
+
+// const server = http.createServer((req,res)=>{
+//   console.log(req,"req")
+//     if(req.method==="GET" && req.url === "/books"){
+//       res.end("books");
+//     }else if (req.method === "GET" && req.url === "/register"){
+//       res.end("register");
+//     }else{
+//       res.end ("Undefined");
+//     }
+// });
+
+// server.listen(3001,()=>{
+//   console.log("server listening on port 3001;");
+// });
+
+
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+  conso-le.log(req,"req");
+  if(req.method === "GET"&& req.url === "/Home"){
+    res.end("Home");
+  }else if (req.method === "GET" && req.url === "/Register"){
+    res.end("Register");
+  }else if (req.method === "GET" && req.url === "/Login"){
+    res.end("Login");
+  }
+  else{
+    res.end("url undefined")
+  }
+
+});
+
+server.listen(3000,()=>{
+  console.log("server listeing on port 3000");
+});
