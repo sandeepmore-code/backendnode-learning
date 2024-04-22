69725d8aa949809b798481275d888bb4055bcb47
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-04-22 20:45:02 +0530

    updates

---
+++ 19-04-2024/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/",(req,res)=>{
+  res.send("Working....")
+});
+
+app.listen(3000,()=>{
+console.log("Server listning on port 3000");
+});
