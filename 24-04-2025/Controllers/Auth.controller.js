d81394c8161a54c616e8082fe75e1254cc98f73d
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-05-25 03:07:43 +0530

    updates

--- 24-04-2025/Controllers/Auth.controller.js
+++ 24-04-2025/Controllers/Auth.controller.js
@@ -1,62 +1,53 @@
 import UserSchema from "../models/users.schema.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"
+
+
 
 
 
 export const Register = async(req,res) => {
   try{
-    const {name,email,password,confirmpassword} = req.body.userData;
-    if(!name || !email || !password || !confirmpassword ){
+    const {name,email,password,confirmpassword} = req.body;
+    if(!name || !email || !password || !confirmpassword){
       return res.json({success : false, message : "All feilds are required"})
     }
-    if(password !== confirmpassword){
-      return res.json({success : false,message : "password not matched!, please Re-enter"});
+    if(password!== confirmpassword){
+      return res.json({success : false, message : "Password not matched"})
     }
-    const isEmailexsists= await UserSchema.findOne({email : email});
-    
-    if(isEmailexsists){
-      return res.json({success : false, message: "Email already exist"})
+    const IsEmailExists = await UserSchema.findOne({email : email})
+    // console.log(IsEmailExists,"IsEmailExist")
+    if(IsEmailExists){
+      return res.json({success : false,message : "Email Already Exist"})
     }
-    const hashedPassword = await bcrypt.hash(password,10);
-    const newUser= new UserSchema({
+    const HashedPassword = await bcrypt.hash(password,10)
+    const newUser = await new UserSchema({
       name : name,
       email : email,
-      password : hashedPassword,
-    });
-    await newUser.save(); 
-    return res.json({success : true, message : "registration completed."})
+      password : HashedPassword
+    })
+    await newUser.save()
+   return res.json({success : true, message : "registration completed"});
   }catch(error){
-    console.log(error)
-    return res.json({error,success: false,})
- 
+    console.log(error,"error")
+      return res.json({error,success : false})
   }
-  
 };
    
 
 export const Login = async(req,res) => {
   try{
     const {email,password} = req.body;
-    if(!email || !password ){
-      return res.json({success : false, message : "all Feilds are required"});
+    if( !email || !password){
+      return res.json({success : false, message : "All feilds are required"})
     }
-    const user = await UserSchema.findOne({email : email})
-    if(!user){
-      return res.json({
-        success : false,
-        message : "user not exist ,please check you email"
-      });
+    const User = await UserSchema.findOne({email : email})
+    if(!User){
+      return res.json({success : false,message : "User Not Exist ,please check your email"})
     }
-    console.log(user,"user");
-    const isPasswordCorrect = await bcrypt.compare(password,user.password);
-
-    if(!isPasswordCorrect){
-      return res.json({success:false,message : "please enter correct password"});
-    }
-    return res.json({success : true,message: "Login Successful"})
+    console.log(User , "User")
   }catch(error){
-    return res.json({Success : false, error})
+    console.log(error,"error")
+      return res.json({error,success : false})
   }
-
 };
 
--- 24-04-2025/models/users.schema.js
+++ 24-04-2025/models/users.schema.js
@@ -1,24 +1,10 @@
-// import { Schema, model } from "mongoose";
-
-// const userSchema = new Schema({
-//   name : String,
-//   email : String,
-//   password : String,
-// });
-
-// const UserSchema = model("user",userSchema);
-
-// export default UserSchema;
-
-import { Schema,model } from "mongoose";
+import { Schema ,model} from "mongoose";
 
 const userSchema = new Schema({
   name : String,
   email : String,
-  password : String
-});
+  password : String,
+})
+const UserSchema = model("User",userSchema);
 
-const UserSchema = model('user',userSchema);
 export default UserSchema;
-
-
