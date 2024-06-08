5d9a0256aa88d45869bd3f54025411c3b007a9cb
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-06-08 13:03:43 +0530

    Update in frontend url

--- day15/index.js
+++ day15/index.js
@@ -12,7 +12,7 @@ import AllRoutes from "./Routes/index.js"
 
 const app = express();
 var corsOptions = {
-  origin : ["http://localhost:3000","https://myntra.com", "https://react-1-three-alpha.vercel.app"],
+  origin : ["http://localhost:3000","https://myntra.com", "https://react-1-three-alpha.vercel.app","https://react-1-woad-five.vercel.app"],
   credentials : true
 };
 app.use(cors(corsOptions)); 
